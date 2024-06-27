dbd6ab1f8b248e1e2c9622d152b7bdf74ef0b1c4
tsuki0530 <tatsukitokushige1995@gmail.com>
2024-06-27 14:24:40 +0000

    Improve the READMEfile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def hello
+    render html:"hello, world!"
+  end
+  
+  def hello2
+    render html:"hola, mundo!"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  root "hello#index"
+  #root "application#hello"
+  root "application#hello2"
 end
